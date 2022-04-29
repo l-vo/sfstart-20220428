65778a2866eba576404c16c43cf6a8d8b7131099
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-04-29 10:12:03 +0200

    Add Movie and Genre entities

---
+++ src/Entity/Movie.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Intl\Countries;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 80)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $poster;
+
+    #[ORM\Column(type: 'string', length: 2)]
+    private $country;
+
+    #[ORM\Column(type: 'date_immutable')]
+    private $released;
+
+    #[ORM\Column(type: 'decimal', precision: 4, scale: 2)]
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(?string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        // Country code validation
+        Countries::getName($country);
+
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getReleased(): ?\DateTimeImmutable
+    {
+        return $this->released;
+    }
+
+    public function setReleased(\DateTimeImmutable $released): self
+    {
+        $this->released = $released;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
