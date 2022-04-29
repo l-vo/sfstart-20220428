af7447b46f213eb7a57ab9a0e7396cc903b25316
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-04-29 15:56:48 +0200

    Add validation on contact form

--- src/Dto/Contact.php
+++ src/Dto/Contact.php
@@ -2,9 +2,15 @@
 
 namespace App\Dto;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class Contact
 {
+    #[Assert\NotBlank]
+    #[Assert\Email]
     public readonly string $email;
+
+    #[Assert\NotBlank]
     public readonly string $message;
 
     public function setEmail(string $email): void
